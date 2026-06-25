ae7b0975f06c7f69f5775142e7f547a98e67cde0
LurnaiHub <official.lurnaihub@gmail.com>
2026-06-25 09:26:25 -0500

    Create Key Api.lua

---
+++ Library/Key System/Key Api.lua
@@ -0,0 +1,17 @@
+return {
+  [GameID] = {
+    ["Linkvertise"] = {
+      Url = "",
+      Icon = 127199596276922,
+    },
+
+    ["LootLabs"] = {
+      Url = "",
+      Icon = 92650912362358,
+    },
+
+    ["Work.ink"] = {
+      Url = "",
+      Icon = 119674855779294,
+    },
+}
