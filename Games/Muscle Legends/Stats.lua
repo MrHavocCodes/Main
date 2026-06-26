b595a0242814ffc2ee770185311910e27869e809
LurnaiHub <official.lurnaihub@gmail.com>
2026-06-26 13:17:27 -0500

    Add Stats module for Muscle Legends

---
+++ Games/Muscle Legends/Stats.lua
@@ -0,0 +1,11 @@
+local Stats = {
+    { name = "Strength",   source = "leaderstats" },
+    { name = "Rebirths",   source = "leaderstats" },
+    { name = "Kills",      source = "leaderstats" },
+    { name = "Brawls",     source = "leaderstats" },
+    { name = "Durability", source = "player"      },
+    { name = "Agility",    source = "player"      },
+    { name = "Gems",       source = "player"      },
+}
+
+return Stats
