1f49f7a79fb66e8e63cf1c3217a5d45ca65c9023
LurnaiHub <official.lurnaihub@gmail.com>
2026-06-26 13:18:00 -0500

    Add codes for Muscle Legends game

---
+++ Games/Muscle Legends/Codes.lua
@@ -0,0 +1,18 @@
+local Codes = {
+    "mightygems2500",
+    "ultimate250",
+    "spacegems50",
+    "megalift50",
+    "speedy50",
+    "epicreward500",
+    "MillionWarriors",
+    "frostgems10",
+    "Musclestorm50",
+    "Skyagility50",
+    "galaxycrystal50",
+    "supermuscle100",
+    "superpunch100",
+    "launch250",
+}
+
+return Codes
