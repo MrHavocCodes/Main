ac069eb18a63d795780036c4430e85c41066e8af
LurnaiHub <official.lurnaihub@gmail.com>
2026-06-26 13:17:44 -0500

    Add Rocks.lua with rock types and durabilities

---
+++ Games/Muscle Legends/Rocks.lua
@@ -0,0 +1,14 @@
+local Rocks = {
+    { name = "Tiny Rock",        durability = 0         },
+    { name = "Punching Rock",    durability = 10        },
+    { name = "Large Rock",       durability = 100       },
+    { name = "Golden Rock",      durability = 5000      },
+    { name = "Frost Rock",       durability = 150000    },
+    { name = "Mythical Rock",    durability = 400000    },
+    { name = "Inferno Rock",     durability = 750000    },
+    { name = "Legend Rock",      durability = 1000000   },
+    { name = "Muscle King Rock", durability = 5000000   },
+    { name = "Jungle Rock",      durability = 10000000  },
+}
+
+return Rocks
