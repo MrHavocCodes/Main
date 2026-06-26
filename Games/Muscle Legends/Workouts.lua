8202624c3db9f7bf1a3efcaf5081c94741dcc428
LurnaiHub <official.lurnaihub@gmail.com>
2026-06-26 13:16:11 -0500

    Create Workouts.lua

---
+++ Games/Muscle Legends/Workouts.lua
@@ -0,0 +1,56 @@
+local Workouts = {
+    {
+        name = "Bar Lift",
+        locations = {
+            "Spawn",
+            "Legend Beach",
+            "Frost Gym",
+            "Legend Gym",
+            "Muscle King Gym",
+            "Jungle Gym",
+        },
+    },
+    {
+        name = "Bench Press",
+        locations = {
+            "Tiny Gym",
+            "Spawn",
+            "Legend Beach",
+            "Frost Gym",
+            "Mythical Gym",
+            "Inferno Gym",
+            "Legend Gym",
+            "Muscle King Gym",
+            "Jungle Gym",
+        },
+    },
+    {
+        name = "Squat",
+        locations = {
+            "Spawn",
+            "Legend Beach",
+            "Frost Gym",
+            "Legend Gym",
+            "Muscle King Gym",
+            "Jungle Gym",
+        },
+    },
+    {
+        name = "Pull Up",
+        locations = {
+            "Spawn",
+            "Mythical Gym",
+            "Legend Gym",
+        },
+    },
+    {
+        name = "Boulder",
+        locations = {
+            "Mythical Gym",
+            "Muscle King Gym",
+            "Jungle Gym",
+        },
+    },
+}
+
+return Workouts
