cb98c2156bde950ec4598533e61fba9fe708b1f9
LurnaiHub <official.lurnaihub@gmail.com>
2026-06-26 13:16:31 -0500

    Create Ultimates.lua

---
+++ Games/Muscle Legends/Ultimates.lua
@@ -0,0 +1,16 @@
+local Ultimates = {
+    { name = "RepSpeed",       reward = "+5% Rep Speed"    },
+    { name = "PetSlot",        reward = "+1 Pet Slot"      },
+    { name = "ItemCapacity",   reward = "+10 Item Capacity" },
+    { name = "DailySpin",      reward = "+1 Daily Spin"    },
+    { name = "ChestRewards",   reward = "x2 Chest Rewards" },
+    { name = "QuestRewards",   reward = "x2 Quest Rewards" },
+    { name = "MuscleMind",     reward = "Muscle Mind"      },
+    { name = "JungleSwift",    reward = "Jungle Swift"     },
+    { name = "InfernalHealth", reward = "Infernal Health"  },
+    { name = "GalaxyGains",    reward = "Galaxy Gains"     },
+    { name = "DemonDamage",    reward = "Demon Damage"     },
+    { name = "GoldenRebirth",  reward = "Golden Rebirth"   },
+}
+
+return Ultimates
