c6d7b68079a17ca55e2bc29085075bfaea0edb75
LurnaiHub <official.lurnaihub@gmail.com>
2026-06-26 13:16:56 -0500

    Add Treadmills data structure for game

---
+++ Games/Muscle Legends/Treadmills.lua
@@ -0,0 +1,11 @@
+local Treadmills = {
+    { name = "Spawn Gym",    agilityReq = 600   },
+    { name = "Legend Beach", agilityReq = 1000  },
+    { name = "Frost Gym",    agilityReq = 3000  },
+    { name = "Mythical Gym", agilityReq = 3000  },
+    { name = "Inferno Gym",  agilityReq = 3500  },
+    { name = "Legend Gym",   agilityReq = 3000  },
+    { name = "Jungle Gym",   agilityReq = 20000 },
+}
+
+return Treadmills
